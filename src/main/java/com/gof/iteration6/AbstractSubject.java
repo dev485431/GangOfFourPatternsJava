85764be6316d36ecc68d6db47bc4b21b6454741b
Tomasz Krzos <tkrzos@dataart.com>
2016-10-19 16:42:21 +0200

    Observer pattern: beginning;

--- src/main/java/com/gof/iteration6/AbstractSubject.java
+++ src/main/java/com/gof/iteration6/AbstractSubject.java
@@ -2,26 +2,22 @@ package com.gof.iteration6;
 
 import com.gof.customer.monitors.Monitor;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public abstract class AbstractSubject {
 
-    protected volatile List<Monitor> observers = new ArrayList<>();
+    protected volatile List<Monitor> observers = new CopyOnWriteArrayList<>();
 
     protected void addObserver(Monitor monitor) {
-        synchronized (observers) {
-            observers.add(monitor);
-        }
+        observers.add(monitor);
     }
 
     protected void removeObserver(Monitor monitor) {
-        synchronized (observers) {
-            observers.remove(monitor);
-        }
+        observers.remove(monitor);
     }
 
-    public synchronized List<Monitor> getObservers() {
+    public List<Monitor> getObservers() {
         return observers;
     }
 }
